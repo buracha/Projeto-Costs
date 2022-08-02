4a545472c1d8e5de2feb30544d72a6fb5551b940
buracha <jadsoncanuto6@gmail.com>
2022-08-02 18:01:35 -0300

    Projeto-Costs

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Home from './components/pages/Home';
+import Company from './components/pages/Company';
+import Contact from './components/pages/Contact';
+import NewProject from './components/pages/NewProject';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <ul>
+        <li>Home</li>
+        <li>Cotato</li>
+      </ul>
+      <Switch>
+        <Route path="/">
+          <Home />
+        </Route>
+
+        <Route path="/company">
+          <Company />
+        </Route>
+
+        <Route path="/contact">
+          <Contact />
+        </Route>
+
+        <Route path="/newproject">
+          <NewProject />
+        </Route>
+      </Switch>
+      <p>Footer</p>
+    </Router>
   );
 }
 
